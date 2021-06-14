894deab68f95c8bc3c505d79b7856faaa3786f6a
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-14 18:41:14 +0100

    Book API

---
+++ database/migrations/2021_06_14_170456_create_books_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // name
+        // isbn
+        // authors 
+        // country
+        // number_of_pages
+        // publisher
+        // release_date
+
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("isbn");
+            $table->json("authors");
+            $table->string("country");
+            $table->integer("number_of_pages");
+            $table->string("publisher");
+            $table->integer("release_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
